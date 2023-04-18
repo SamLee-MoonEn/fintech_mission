0c170eb784a7be8e4e4b71da87e18d0eef77a151
SamLee <goha2010@naver.com>
2023-04-19 08:01:53 +0900

    Navbar 구조 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
 function App() {
-  return <div></div>
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <div className='max-w-[80%]'></div>
+      <div className="fixed bottom-0">Footer</div>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+export default function Navbar() {
+  return (
+    <div className="fixed w-full h-12 border-solid border-gray-300 border-b flex justify-center items-center text-gray-500">
+      <div className="max-w-[80%] w-full flex justify-between items-center">
+        <a href="/" className="">
+          MoonEn
+        </a>
+        <input id="drawer" type="checkbox" className="hidden toggle" />
+        <label htmlFor="drawer" className=" cursor-pointer p-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block w-5 h-5 stroke-gray-500"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </label>
+        <ul className="sidebar hidden">
+          <li>회원가입</li>
+          <li>구글연동</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
