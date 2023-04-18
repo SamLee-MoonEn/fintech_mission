8655ef1c2ad91c4a10662353afbf81ebe5d192a6
SamLee <goha2010@naver.com>
2023-04-18 22:53:19 +0900

    프로젝트 초기 설정 완료

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1025px',
+      xl: '1280px',
+      xl2: '1360px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['GmarketSansMedium'],
+      },
+    },
+  },
+  plugins: [],
+}
