3750f31160f1576470350f03bba64a7f0bede3c9
SamLee <goha2010@naver.com>
2023-05-02 22:23:45 +0900

    Session Storage에 user UID 저장하도록 변경.

--- src/store/userInfo.ts
+++ src/store/userInfo.ts
@@ -10,4 +10,11 @@ export const authState = atom({
 export const userState = atom({
   key: 'user',
   default: '',
+  effects: [
+    ({ setSelf, onSet }) => {
+      sessionStorage.getItem('USER_UID') &&
+        setSelf(sessionStorage.getItem('USER_UID') as string)
+      onSet((v) => sessionStorage.setItem('USER_UID', `${v}`))
+    },
+  ],
 })
