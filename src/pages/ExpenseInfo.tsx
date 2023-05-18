c1cbdab6bf98e015e2cd204ef496cb8b3deeaf1f
SamLee <goha2010@naver.com>
2023-05-18 23:53:41 +0900

    지출 정보 그래프 개선.(계좌 번호 추가)

--- src/pages/ExpenseInfo.tsx
+++ src/pages/ExpenseInfo.tsx
@@ -1,37 +1,29 @@
-import { useEffect, useState } from 'react'
 import { useRecoilValueLoadable } from 'recoil'
 
-import { userState, userTransectionsInfo } from '../store/userInfo'
+import { userAccountList, userTransectionsInfo } from '../store/userInfo'
 import ChartCard from '../components/ChartCard'
 
 export default function ExpenseInfo() {
   const userTransectionsInfoLoadable =
     useRecoilValueLoadable(userTransectionsInfo)
-  const [transectionList, setTransectionList]: any = useState([
-    '날짜',
-    '입금',
-    '출금',
-    '지출',
-  ])
+  const accountList = useRecoilValueLoadable(userAccountList)
   const transectionDataList =
     userTransectionsInfoLoadable.state === 'hasValue'
       ? userTransectionsInfoLoadable.contents
       : []
-
-  // useEffect(() => {
-  //   if (userTransectionsInfoLoadable.state === 'hasValue') {
-  //     const transectionDataList = userTransectionsInfoLoadable.contents
-  //     setTransectionList([[...transectionList], ...transectionDataList[0]])
-  //   }
-  //   console.log(transectionList)
-  // }, [userTransectionsInfoLoadable])
+  const accounts =
+    accountList.state === 'hasValue' ? Object.keys(accountList.contents) : []
 
   return (
     <div className="max-w-full md:max-w-[80%] ml-auto mr-auto">
       <div className="flex w-full h-96 items-center mt-24 flex-col">
         <div className="w-8/12">
           {transectionDataList.map((v, idx) => (
-            <ChartCard transectionsData={v} key={idx} />
+            <ChartCard
+              transectionsData={v}
+              accountNum={accounts[idx]}
+              key={idx}
+            />
           ))}
         </div>
       </div>
