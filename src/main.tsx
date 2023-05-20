1dac564e373994076b3b5feb54e42921e69bbf0c
SamLee <goha2010@naver.com>
2023-05-20 22:57:41 +0900

    react query를 사용하기 위해서 root 페이지에서 전체 페이지를 감싸는 코드 작성.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RecoilRoot } from 'recoil'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import App from './App'
 import './index.css'
 
+const qeuryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
+    <QueryClientProvider client={qeuryClient}>
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
