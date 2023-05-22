5d22a33dfeb31c26ce0fa225d93fef946d6b7610
SamLee <goha2010@naver.com>
2023-05-22 21:58:54 +0900

    CORS 에러 해결을 위해  Proxy 설정함.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target:
+          'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
