e8ef4796228a6d46fa6583bdc4aea96aba886f8d
SamLee <goha2010@naver.com>
2023-05-25 16:43:36 +0900

    주식, 환율 추가 모달 생성.

--- src/components/modals/ShortCutAddModal.tsx
+++ src/components/modals/ShortCutAddModal.tsx
@@ -4,6 +4,8 @@ import { userState } from '../../store/userInfo'
 import {
   getAccountInfo,
   setShortcutDataToFirebase,
+  getInterestedStockInfoFromFirebase,
+  getInterestedExchangeRateFromFirebase,
 } from '../../helper/firebaseAuth'
 import { useEffect, useState } from 'react'
 
@@ -33,8 +35,12 @@ export default function ShortCutAddModal({
             <label htmlFor="addExpenseShortcutCard" className="btn text-xl">
               지출 카드
             </label>
-            <label className="btn text-xl">주식 카드</label>
-            <label className="btn text-xl">환율 카드</label>
+            <label htmlFor="addStockShortcutCard" className="btn text-xl">
+              주식 카드
+            </label>
+            <label htmlFor="addExchangRateShortcutCard" className="btn text-xl">
+              환율 카드
+            </label>
             <label htmlFor="addQRShortcutCard" className="btn text-xl">
               결제 카드
             </label>
@@ -50,6 +56,14 @@ export default function ShortCutAddModal({
         updateShortcutList={updateShortcutList}
       />
       <AddQRhortcutCard
+        userUid={userUid}
+        updateShortcutList={updateShortcutList}
+      />
+      <AddStockShortcutCard
+        userUid={userUid}
+        updateShortcutList={updateShortcutList}
+      />
+      <AddExchangeRateShortcutCard
         userUid={userUid}
         updateShortcutList={updateShortcutList}
       />
@@ -119,7 +133,11 @@ function AddAccountShortcutCard({
                 계좌번호
               </option>
               {accountInfo?.map((v) => {
-                return <option value={v.accountNum}>{v.accountNum}</option>
+                return (
+                  <option key={v.accountNum} value={v.accountNum}>
+                    {v.accountNum}
+                  </option>
+                )
               })}
             </select>
           </form>
@@ -196,7 +214,11 @@ function AddExpenseShortcutCard({
                 계좌번호
               </option>
               {accountInfo?.map((v) => {
-                return <option value={v.accountNum}>{v.accountNum}</option>
+                return (
+                  <option key={v.accountNum} value={v.accountNum}>
+                    {v.accountNum}
+                  </option>
+                )
               })}
             </select>
           </form>
@@ -214,6 +236,7 @@ function AddExpenseShortcutCard({
     </>
   )
 }
+
 function AddQRhortcutCard({
   userUid,
   updateShortcutList,
@@ -268,7 +291,11 @@ function AddQRhortcutCard({
                 계좌번호
               </option>
               {accountInfo?.map((v) => {
-                return <option value={v.accountNum}>{v.accountNum}</option>
+                return (
+                  <option key={v.accountNum} value={v.accountNum}>
+                    {v.accountNum}
+                  </option>
+                )
               })}
             </select>
           </form>
@@ -276,6 +303,179 @@ function AddQRhortcutCard({
             <label
               onClick={handleAddShortCutExpenseCard}
               htmlFor="addQRShortcutCard"
+              className="btn"
+            >
+              카드 추가
+            </label>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+interface StockTypes {
+  종목명: string
+  종목코드: string
+}
+
+function AddStockShortcutCard({
+  userUid,
+  updateShortcutList,
+}: {
+  userUid: string
+  updateShortcutList: () => Promise<void>
+}) {
+  const [stockInfo, setStockInfo] = useState<StockTypes[]>()
+  const [selectedStock, setSelectedStock] = useState<string>()
+
+  const handleGetStockInfo = async () => {
+    const temp = await getInterestedStockInfoFromFirebase(userUid)
+    setStockInfo(Object.values(temp))
+  }
+  const handlesetSelectedStock = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedStock(e.target.value)
+  }
+
+  const handleAddShortCutStock = () => {
+    if (!selectedStock) {
+      alert('관심 주식을 선택해주세요.')
+      return
+    }
+    setShortcutDataToFirebase(userUid, 'Stock', selectedStock)
+    updateShortcutList()
+  }
+
+  useEffect(() => {
+    handleGetStockInfo()
+  }, [])
+  return (
+    <>
+      <input
+        type="checkbox"
+        id="addStockShortcutCard"
+        className="modal-toggle"
+      />
+      <div className="modal">
+        <div className="modal-box flex flex-col items-center">
+          <h1 className="font-bold text-3xl w-full text-center">
+            관심 주식 카드
+          </h1>
+          <label
+            htmlFor="addStockShortcutCard"
+            className="btn btn-sm btn-circle absolute right-3 top-3"
+          >
+            ✕
+          </label>
+          <div className="divider"></div>
+          <h3 className="font-bold text-xl mb-4">
+            카드로 추가할 관심 주식을 선택해 주세요.
+          </h3>
+          <form>
+            <select onChange={handlesetSelectedStock}>
+              <option value="default" hidden selected disabled>
+                관심 주식
+              </option>
+              {stockInfo?.map((v) => {
+                return (
+                  <option key={v.종목코드} value={v.종목코드}>
+                    {v.종목코드} {v.종목명}
+                  </option>
+                )
+              })}
+            </select>
+          </form>
+          <div className="modal-action">
+            <label
+              onClick={handleAddShortCutStock}
+              htmlFor="addStockShortcutCard"
+              className="btn"
+            >
+              카드 추가
+            </label>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+interface ExchageRateTypes {
+  환율코드: string
+  환율명: string
+}
+
+function AddExchangeRateShortcutCard({
+  userUid,
+  updateShortcutList,
+}: {
+  userUid: string
+  updateShortcutList: () => Promise<void>
+}) {
+  const [exchangeRateInfo, setExchangeRateInfo] = useState<ExchageRateTypes[]>()
+  const [selectedExchangeRate, setSelectedExchangeRate] = useState<string>()
+
+  const handleGetExchageRateInfo = async () => {
+    const temp = await getInterestedExchangeRateFromFirebase(userUid)
+    setExchangeRateInfo(Object.values(temp))
+  }
+  const handlesetSelectedExchangeRate = (
+    e: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setSelectedExchangeRate(e.target.value)
+  }
+
+  const handleAddShortCutExchangeRate = () => {
+    if (!selectedExchangeRate) {
+      alert('관심 환율을 선택해주세요.')
+      return
+    }
+    setShortcutDataToFirebase(userUid, 'Currency', selectedExchangeRate)
+    updateShortcutList()
+  }
+
+  useEffect(() => {
+    handleGetExchageRateInfo()
+  }, [])
+  return (
+    <>
+      <input
+        type="checkbox"
+        id="addExchangRateShortcutCard"
+        className="modal-toggle"
+      />
+      <div className="modal">
+        <div className="modal-box flex flex-col items-center">
+          <h1 className="font-bold text-3xl w-full text-center">
+            관심 환율 카드
+          </h1>
+          <label
+            htmlFor="addExchangRateShortcutCard"
+            className="btn btn-sm btn-circle absolute right-3 top-3"
+          >
+            ✕
+          </label>
+          <div className="divider"></div>
+          <h3 className="font-bold text-xl mb-4">
+            카드로 추가할 관심 환율을 선택해 주세요.
+          </h3>
+          <form>
+            <select onChange={handlesetSelectedExchangeRate}>
+              <option value="default" hidden selected disabled>
+                관심 환율
+              </option>
+              {exchangeRateInfo?.map((v) => {
+                return (
+                  <option key={v.환율코드} value={v.환율코드}>
+                    {v.환율코드} {v.환율명}
+                  </option>
+                )
+              })}
+            </select>
+          </form>
+          <div className="modal-action">
+            <label
+              onClick={handleAddShortCutExchangeRate}
+              htmlFor="addExchangRateShortcutCard"
               className="btn"
             >
               카드 추가
